20ce80d84fda4e5c67f3b8197e45fa98f16ec267
Codey Suh <codeysuh2002@gmail.com>
2024-12-15 01:24:24 -0800

    frontend_v1 improved and connected with backend

--- frontend/app.js
+++ frontend/app.js
@@ -1,108 +1,22 @@
-const player1Stats = ["91%", "72%", "63%", "7", "0", "2", "25", "186cm", "83kg"]
+const searchPlayer = async () => {
+    const playerName = document.querySelector('.search__bar').value
 
-const matches = 
-    [
-        {
-            player1Name: "Francis Tiafoe",
-            player2Name: "Jannik Sinner",
-            player1Rank: "Rank 3",
-            player2Rank: "Rank 5",
-            tournament: "Wimbledon",
-            round: "R16",
-            date: "03/11/24",
-            result: "Loss",
-            time: "182 minutes",
-            score: "6-3 4-6 6-2"
-        },
-        {
-            player1Name: "Francis Tiafoe",
-            player2Name: "Taylor Fritz",
-            player1Rank: "Rank 3",
-            player2Rank: "Rank 5",
-            tournament: "Wimbledon",
-            round: "R32",
-            date: "03/11/24",
-            result: "Win",
-            time: "120 minutes",
-            score: "6-3 6-4"
-        },
-        {
-            player1Name: "Francis Tiafoe",
-            player2Name: "Jannik Sinner",
-            player1Rank: "Rank 3",
-            player2Rank: "Rank 5",
-            tournament: "Wimbledon",
-            round: "R16",
-            date: "03/11/24",
-            result: "Loss",
-            time: "182 minutes",
-            score: "6-3 4-6 6-2"
-        },
-        {
-            player1Name: "Francis Tiafoe",
-            player2Name: "Taylor Fritz",
-            player1Rank: "Rank 3",
-            player2Rank: "Rank 5",
-            tournament: "Wimbledon",
-            round: "R32",
-            date: "03/11/24",
-            result: "Win",
-            time: "120 minutes",
-            score: "6-3 6-4"
-        },
-        {
-            player1Name: "Francis Tiafoe",
-            player2Name: "Jannik Sinner",
-            player1Rank: "Rank 3",
-            player2Rank: "Rank 5",
-            tournament: "Wimbledon",
-            round: "R16",
-            date: "03/11/24",
-            result: "Loss",
-            time: "182 minutes",
-            score: "6-3 4-6 6-2"
-        },
-        {
-            player1Name: "Francis Tiafoe",
-            player2Name: "Taylor Fritz",
-            player1Rank: "Rank 3",
-            player2Rank: "Rank 5",
-            tournament: "Wimbledon",
-            round: "R32",
-            date: "03/11/24",
-            result: "Win",
-            time: "120 minutes",
-            score: "6-3 6-4"
+    let url = `http://localhost:3000/player/getPlayerId/${playerName}`
+    let playerId
+    try {
+        const response = await fetch(url)
+        if (!response.ok) {
+            if (response.status === 404) {
+                window.location.assign(`http://localhost:3000/noplayer`)
+            } else {
+                throw new Error(`Response status: ${response.status}`)
+            }
         }
-    ]
 
-// General Player Stats
-const statsElements = document.querySelectorAll(".stats__text");
-for (let i = 0; i < statsElements.length; i++) {
-    statsElements[i].innerHTML += player1Stats[i];
+        const json = await response.json()
+        playerId = json.playerId
+        window.location.assign(`http://localhost:3000/player/${playerId}`)
+    } catch (err) {
+        console.error(err);
+    }
 }
-
-// Match Stats
-const updateElements = (classVariablePairs) => {
-    classVariablePairs.forEach(({ className, variable }) => {
-        const elements = document.querySelectorAll(className);
-        for (let i = 0; i < elements.length; i++) {
-            elements[i].innerHTML = matches[i][variable];
-        }
-    });
-};
-
-updateElements([
-    { className: ".player1__name", variable: "player1Name" },
-    { className: ".player2__name", variable: "player2Name" },
-    { className: ".player1__rank", variable: "player1Rank" },
-    { className: ".player2__rank", variable: "player2Rank" },
-    { className: ".match__tournament", variable: "tournament" },
-    { className: ".match__date", variable: "date" },
-    { className: ".match__result", variable: "result" },
-    { className: ".match__length", variable: "time" },
-    { className: ".match__round", variable: "round" },
-    { className: ".match__score", variable: "score" },
-]);
-
-
